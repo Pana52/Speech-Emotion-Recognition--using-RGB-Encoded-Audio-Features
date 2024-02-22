3c9daf38842c1ed5ff78ab28ae497fc74f921451
Panagiotis Theodorou <panayiotisth71@gmail.com>
2024-02-22 11:16:49 +0000

    KNN

---
+++ models/machine learning/KNN/model.py
@@ -0,0 +1,16 @@
+# model.py
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class KNNModel:
+    def __init__(self, n_neighbors=5):
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    def evaluate(self, X_test, y_test):
+        return self.model.score(X_test, y_test)
---
+++ models/machine learning/KNN/train.py
@@ -0,0 +1,20 @@
+# train.py
+from preprocessing import load_data
+from model import KNNModel
+
+# Assuming the CREMA-D dataset is located in 'data/CREMA-D' directory
+data_path = "C:/Users/Pana/Desktop/Northumbria/Final Year/Individual Computing Project KV6003BNN01/datasets/CREMAD/"
+
+
+def main():
+    X_train, X_test, y_train, y_test = load_data(data_path)
+
+    knn_model = KNNModel(n_neighbors=5)
+    knn_model.train(X_train, y_train)
+
+    accuracy = knn_model.evaluate(X_test, y_test)
+    print(f'Model accuracy: {accuracy * 100:.2f}%')
+
+
+if __name__ == '__main__':
+    main()
